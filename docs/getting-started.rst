cbade0f24201ac88e10a6dba35e8c72c049271a8
Genevieve Flaspohler <geflaspo@mit.edu>
2021-04-15 09:37:12 -1000

    First commit of cookiecutter code

---
+++ docs/getting-started.rst
@@ -0,0 +1,5 @@
+Getting started
+===============
+
+This is where you describe how to get set up on a clean install, including the
+commands necessary to get the raw data, and then how to make the cleaned, final data sets.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python library for Optimistic Online Learning under Delay',
+    author='Genevieve Flaspohler',
+    license='MIT',
+)
