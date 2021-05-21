a9813d1a9c9345b9dd90f57eddc4c9a8e218c0ad
Genevieve Flaspohler <geflaspo@mit.edu>
2021-05-21 12:34:37 -0400

    poold repo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='poold',
     packages=find_packages(),
     version='0.1.0',
     description='Python library for Optimistic Online Learning under Delay',
